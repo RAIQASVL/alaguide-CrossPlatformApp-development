dc7474694d2f1097d3838d9442690f1806f76b11
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-04-19 02:36:48 +0500

    Initialized commit

---
+++ database_design/alaguide_db.sql
@@ -0,0 +1,105 @@
+DROP DATABASE IF EXISTS 'alaguide_db';
+
+CREATE DATABASE 'alaguide_db';
+
+USE 'alaguide_db';
+
+SET
+    NAMES utf8;
+
+SET
+    character_set_client = utf8mb4;
+
+-- Table for countries
+CREATE TABLE
+    Countries (
+        country_id INT PRIMARY KEY AUTO_INCREMENT,
+        countryname VARCHAR(100) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for cities
+CREATE TABLE
+    Cities (
+        city_id INT PRIMARY KEY AUTO_INCREMENT,
+        cityname VARCHAR(100) NOT NULL,
+        description TEXT,
+        latitude DECIMAL(9, 6),
+        longitude DECIMAL(9, 6),
+        country_id INT,
+        FOREIGN KEY (country_id) REFERENCES Countries (country_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for landmarks
+CREATE TABLE
+    Landmarks (
+        landmark_id INT PRIMARY KEY AUTO_INCREMENT,
+        landmarkname VARCHAR(100) NOT NULL,
+        description TEXT,
+        image_url VARCHAR(255),
+        latitude DECIMAL(9, 6),
+        longitude DECIMAL(9, 6),
+        city_id INT,
+        FOREIGN KEY (city_id) REFERENCES Cities (city_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for audiobooks
+CREATE TABLE
+    AudioBooks (
+        audiobook_id INT PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        audio_url VARCHAR(255),
+        landmark_id INT,
+        FOREIGN KEY (landmark_id) REFERENCES Landmarks (landmark_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for user reviews
+CREATE TABLE
+    UserReviews (
+        review_id INT PRIMARY KEY AUTO_INCREMENT,
+        text TEXT,
+        rating DECIMAL(2, 1),
+        date_posted DATE,
+        user_id INT, -- If user authentication is required
+        landmark_id INT,
+        FOREIGN KEY (user_id) REFERENCES Users (user_id), -- If there is a users table
+        FOREIGN KEY (landmark_id) REFERENCES Landmarks (landmark_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for users (if required)
+CREATE TABLE
+    Users (
+        user_id INT PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for likes and raitings (if required)
+CREATE TABLE
+    LikesRatings (
+        like_rating_id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT,
+        landmark_id INT,
+        type ENUM ('like', 'rating'),
+        date_liked_or_rated DATE,
+        FOREIGN KEY (user_id) REFERENCES Users (user_id),
+        FOREIGN KEY (landmark_id) REFERENCES Landmark (landmark_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for tags (if required)
+CREATE TABLE
+    Tags (
+        tag_id INT PRIMARY KEY AUTO_INCREMENT,
+        tagname VARCHAR(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- Table for many-to-many ralationship between landmarks and tags (if required)
+CREATE TABLE
+    LandmarkTags (
+        landmark_id INT,
+        tag_id INT,
+        PRIMARY KEY (landmark_id, tag_id),
+        FOREIGN KEY (landmark_id) REFERENCES Landmarks (landmark_id),
+        FOREIGN KEY (tag_id) REFERENCES Tags (tag_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
