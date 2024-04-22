f576224ad0b9f09f3871783d818659bc3da55f05
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-04-23 03:24:41 +0500

    Database (alaguide_db) Init

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "alaguide_db",
+        "USER": "root",
+        "PASSWORD": "aquonelocal108",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
