98895f6ece3066b9e13aba7b524dc982237eb4db
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-05-14 00:07:10 +0500

    Created Endpoints for APIs | API Status - ProtoStructure

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
-from .views import ListAlaguideObject, DetailAlaguideObject
+from .views import (
+    ListAlaguideObject,
+    DetailAlaguideObject,
+    MenuView,
+    LocationMenuView,
+    ContentMenuView,
+    LanguageMenuView,
+    AboutView,
+    SupportView,
+    FeedbackView,
+)
+
 
 urlpatterns = [
+    # 1. The Main AlaguideObject View ("guide/objects/")
     path("", ListAlaguideObject.as_view()),
-    path("<int:pk>/", DetailAlaguideObject.as_view())
+    path("<int:pk>/", DetailAlaguideObject.as_view()),
+    # 2. Menu View ("guide/menu/")
+    path("guide/menu/", MenuView.as_view()),
+    # 3. Menu View: Sub Category "Location" ("guide/menu/location/")
+    path("guide/menu/location/", LocationMenuView.as_view()),
+    # 4. Menu View: Sub Category "Content" ("guide/menu/content/")
+    path("guide/menu/content/", ContentMenuView.as_view()),
+    # 5. Menu View: Sub Category "Language" ("guide/menu/language/")
+    path("guide/menu/language/", LanguageMenuView.as_view()),
+    # 6. Menu View: Sub Category "About" ("guide/menu/about/")
+    path("guide/menu/about/", AboutView.as_view()),
+    # 7. Menu View: Sub Category "Support" ("guide/menu/support/")
+    path("guide/menu/support/", SupportView.as_view()),
+    # 8. Menu View: Sub Category "Feedback" ("guide/menu/feedback/")
+    path("guide/menu/feedback/", FeedbackView.as_view()),
 ]
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -11,22 +11,19 @@ urlpatterns = [
     path("", include("core.urls")),
     # REST API
     path("guide/objects/", include("apis.urls")),
-    # path("guide/objects/ala_object_id/", include("apis.urls")),
-    # path("guide/menu/", include("apis.urls")),
-    # path("guide/menu/content/", include("apis.urls")),
-    # path("guide/menu/language/", include("apis.urls")),
-    # path("guide/menu/about", include("apis.urls")),
-    # path("guide/menu/support", include("apis.urls")),
-    # path("guide/menu/feedback/", include("apis.urls")),
-    # path("guide/cities", include("apis.urls")),
-    # path("guide/cities/city_id/content/", include("apis.urls")),
-    # path("guide/cities/city_id/content/search", include("apis.urls")),
+    ## Menu Structure
+    path("guide/menu/", include("apis.urls")),
+    path("guide/menu/location/", include("apis.urls")),
+    path("guide/menu/content/", include("apis.urls")),
+    path("guide/menu/language/", include("apis.urls")),
+    path("guide/menu/about", include("apis.urls")),
+    path("guide/menu/support", include("apis.urls")),
+    path("guide/menu/feedback/", include("apis.urls")),
     # Django Allauth
-    path("home/", TemplateView.as_view(template_name="dashboard/home.html"), name="home"),
+    path(
+        "home/", TemplateView.as_view(template_name="dashboard/home.html"), name="home"
+    ),
     path("accounts/", include("allauth.urls")),
     path("accounts/login/", LoginView.as_view(), name="account_login"),
     path("accounts/signup/", SignupView.as_view(), name="account_signup"),
-    # Google Maps API (Place these in core app's urls.py)
-    # path("map/", views.map_view, name="map-view"),
-    # path("api/landmarks/", views.get_landmarks, name="landmarks-api"),
 ]
