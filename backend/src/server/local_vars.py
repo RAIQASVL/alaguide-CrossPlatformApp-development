dcd56d7530c6d825ce10592ee145e816c3710993
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-05-23 00:44:16 +0500

    Conventional Reassembly

---
+++ backend/src/server/local_vars.py
@@ -0,0 +1,6 @@
+SECRET_KEY = 'django-insecure-turgufs_96v_e!&mbi2#g(8pt_c8_g1kzt-#oel#9ss1(uhrol'
+MYSQL_TEST_NAME = 'test_alaguide_db' # as a MYSQL_TEST_DB in .mysql-env
+MYSQL_NAME = 'alaguide_db'
+MYSQL_USER = 'aquonelocal' # as in  MYSQL_USER in .mysql-env
+MYSQL_PASSWORD = 'Hydrogenium@108' # as a MYSQL_PASSWORD  in .mysql-env
+MYSQL_HOST = 'mysql-db' # as the DB's service name in docker-compose.yml
