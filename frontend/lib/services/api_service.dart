dbd75706c7e43265e3059dde10252c85fceb0632
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-08-12 04:34:29 +0500

    Setup City Model and Current City Page Tab

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -6,8 +6,7 @@ class ApiService {
   ApiService() {
     _dio = Dio(BaseOptions(
       // baseUrl: 'http://10.0.2.2:8000',  // Use this for Android emulator
-      baseUrl:
-          'http://192.168.1.235:8000/', // Use this for iOS simulator or web
+      baseUrl: 'http://192.168.1.235:8000', // Use this for iOS simulator or web
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 10),
     ));
---
+++ frontend/lib/services/city_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import "package:frontend/models/city_model.dart";
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CityService {
+  static const String baseUrl = 'http://192.168.1.235:8000/api/v1';
+
+  Future<List<City>> fetchCities() async {
+    try {
+      print('Fetching cities from: $baseUrl/cities/');
+      final response = await http.get(Uri.parse('$baseUrl/cities/'));
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        List<dynamic> citiesJson = json.decode(response.body);
+        print('Parsed JSON: $citiesJson');
+
+        List<City> cities = citiesJson.map((cityJson) {
+          print('Processing city: $cityJson');
+          return City.fromJson(cityJson);
+        }).toList();
+
+        print('Processed cities: $cities');
+        return cities;
+      } else {
+        throw Exception('Failed to load cities: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching cities: $e');
+      throw Exception('Failed to load cities: $e');
+    }
+  }
+}
