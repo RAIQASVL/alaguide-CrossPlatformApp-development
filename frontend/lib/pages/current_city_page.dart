e3f6649e74414496173062395061c8a4ea951751
RAIQASVL <143160215+RAIQASVL@users.noreply.github.com>
2024-08-18 18:26:23 +0500

    Current City Tab - UI Updated

--- frontend/lib/pages/current_city_page.dart
+++ frontend/lib/pages/current_city_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import "package:frontend/models/city_model.dart";
 import 'package:frontend/providers/city_provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -28,10 +29,21 @@ class CurrentCityPage extends ConsumerWidget {
 
   Widget _buildCityListTile(BuildContext context, WidgetRef ref, City city) {
     final selectedCity = ref.watch(selectedCityProvider);
+    final bool isSelected = selectedCity?.cityId == city.cityId;
+    final Color customColor = Color(0xFF5AD1E5);
+
     return ListTile(
-      title: Text(city.name ?? 'Unknown'),
-      subtitle: Text(city.country ?? 'Unknown'),
-      trailing: selectedCity?.cityId == city.cityId ? Icon(Icons.check) : null,
+      leading: SvgPicture.asset('assets/images/alaguide_ui_icon.svg',
+          color: isSelected ? customColor : Colors.grey),
+      title: Text(city.name ?? 'Unknown',
+          style: TextStyle(
+              color: isSelected ? customColor : Colors.grey,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal)),
+      subtitle: Text(city.country ?? 'Unknown',
+          style: TextStyle(
+            color: isSelected ? Colors.black : Colors.grey,
+          )),
+      trailing: isSelected ? Icon(Icons.check, color: customColor) : null,
       onTap: () {
         print('Selected city: ${city.toString()}');
         ref.read(selectedCityProvider.notifier).state = city;
